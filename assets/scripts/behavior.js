7b2835ea8d82e1458bb597b06da0e22aba29c221
Pythonidaer <pythonidaer@gmail.com>
2021-02-07 22:45:42 -0500

    Found JavaScript from Bulma Tutorial but would like to figure out how to refactor it in jQuery. Succeeded in getting project cards within Bulma tabs, but it looks pretty copout for now. Also, I could definitely create better images. The next thing I have to focus on however is the nesting of the grids

--- assets/scripts/behavior.js
+++ assets/scripts/behavior.js
@@ -7,4 +7,65 @@ $( document ).ready(function() {
         $('.navbar-end').toggleClass('is-flex-direction-column');
         $('.navbar-end .navbar-item').toggleClass('is-align-self-flex-end');
     });
+
+
+
+
+    // tabs
+    const tabs = document.querySelectorAll('.tabs li');
+    const tabContentBoxes = document.querySelectorAll('#tab-content > div');
+
+    tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        tabs.forEach(item => item.classList.remove('is-active'));
+        tab.classList.add('is-active');
+
+        const target = tab.dataset.target;
+        // console.log(target);
+        tabContentBoxes.forEach(box => {
+        if (box.getAttribute('id') === target) {
+            box.classList.remove('is-hidden');
+        } else {
+            box.classList.add('is-hidden');
+        }
+        })
+    })
+    
+})
+
+    // tabs = $('.tabs li');
+    // tabContentBoxes = $('#tab-content > div');
+
+    // tabs.each(function() {
+    //     $(this).on('click', function() {
+    //         tabs.each(function() {
+    //             $(this).removeClass('is-active');
+    //         })
+    //         $(this).addClass('is-active');
+
+    //     tabContentBoxes.each(function() {
+    //         // this is wrong
+    //         if ($(this).attr('id') === $(this).attr('data-target')) {
+    //             $(this).removeClass('is-hidden');
+    //         } else {
+    //             $(this).addClass('is-hidden');
+    //             console.log($(this))
+    //             console.log($(this).attr('id'));
+    //         }
+    //     })
+            // console.log($(this));
+            // console.log($(this).dataset.target);
+            // const target = $(this).dataset.target;
+            // console.log(target);
+//         })
+//     })
+// });
+
+
+
+
+
+
+
+
 });
